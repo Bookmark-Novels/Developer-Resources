7f86d59f984a73261dcaae371b8ea4cfbb41a6d3
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-10 00:50:44 -0700

    add vagrant bootstrap script

--- Tools/Scripts/vagrant_bootstrap.sh
+++ Tools/Scripts/vagrant_bootstrap.sh
@@ -1,5 +1,9 @@
-apt-get install -y git
+VAGRANT_HOME=/home/vagrant
 
+apt-get update
+
+apt-get install -y git
+apt-get install -y python3-pip
 apt-get install -y zsh
 
 su vagrant
@@ -7,4 +11,3 @@ su vagrant
 wget --quiet https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 wget --quiet https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Configuration%20Files/.zshrc -O $VAGRANT_HOME/.zshrc
 chsh -s /bin/zsh vagrant
-zsh
