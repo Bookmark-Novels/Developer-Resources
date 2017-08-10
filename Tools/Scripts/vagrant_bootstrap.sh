9fb04f334ad4e40bb5e4e83fbf39318405bb204c
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-10 01:01:38 -0700

    update

--- Tools/Scripts/vagrant_bootstrap.sh
+++ Tools/Scripts/vagrant_bootstrap.sh
@@ -10,6 +10,11 @@ su vagrant
 
 cd /home/vagrant
 
-wget --quiet https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+if [ ! -d ~vagrant/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
+fi
+
+chown vagrant: ~vagrant/.zshrc
+
 wget --quiet https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Configuration%20Files/.zshrc -O $VAGRANT_HOME/.zshrc
 chsh -s /bin/zsh vagrant
