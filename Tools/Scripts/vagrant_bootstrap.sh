8d1c671123d09f8265d93ef35a8d8140b5977121
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-10 01:02:54 -0700

    remove variable

--- Tools/Scripts/vagrant_bootstrap.sh
+++ Tools/Scripts/vagrant_bootstrap.sh
@@ -1,14 +1,8 @@
-VAGRANT_HOME=/home/vagrant
-
 apt-get update
 
 apt-get install -y git
 apt-get install -y python3-pip
 apt-get install -y zsh
-
-su vagrant
-
-cd /home/vagrant
 
 if [ ! -d ~vagrant/.oh-my-zsh ]; then
   git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
@@ -16,5 +10,5 @@ fi
 
 chown vagrant: ~vagrant/.zshrc
 
-wget --quiet https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Configuration%20Files/.zshrc -O $VAGRANT_HOME/.zshrc
+wget --quiet https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Configuration%20Files/.zshrc -O ~vagrant/.zshrc
 chsh -s /bin/zsh vagrant
