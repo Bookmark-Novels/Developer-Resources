2f968b26b64f0d2cdf81a1a2d55ba47219327fec
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-10 20:59:21 -0700

    update

--- Tools/Scripts/vagrant_bootstrap.sh
+++ Tools/Scripts/vagrant_bootstrap.sh
@@ -8,10 +8,10 @@ if [ ! -d ~vagrant/.oh-my-zsh ]; then
   git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
 fi
 
-chown vagrant: ~vagrant/.zshrc
-
 wget --quiet https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Configuration%20Files/.zshrc -O ~vagrant/.zshrc
 chsh -s /bin/zsh vagrant
+
+chown vagrant: ~vagrant/.zshrc
 
 latest_provision_script=$(curl -s https://raw.githubusercontent.com/Bookmark-Novels/Resources/master/Tools/Scripts/vagrant_bootstrap.sh | md5sum | awk '{print $1}')
 echo $latest_provision_script > ~vagrant/.bookmark_global_vagrant
